1601e624e346ce7fb3ac62d99694ed563104d3a9
Luke Barnett <luke@barnett.net.nz>
2018-03-10 10:32:25 +1300

    Basic volume control

---
+++ Remote.Volume/Controllers/VolumeController.cs
@@ -0,0 +1,36 @@
+﻿using AudioSwitcher.AudioApi;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Remote.Volume.Controllers
+{
+	[Produces("application/json")]
+	[Route("api/volume/[action]")]
+	public class VolumeController : Controller
+	{
+		private readonly IAudioController _audioController;
+
+		public VolumeController(IAudioController audioController)
+		{
+			_audioController = audioController;
+		}
+
+		public async Task<IActionResult> Mute()
+		{
+			await _audioController.DefaultPlaybackDevice.SetMuteAsync(true);
+			return Ok();
+		}
+
+		public async Task<IActionResult> Unmute()
+		{
+			await _audioController.DefaultPlaybackDevice.SetMuteAsync(false);
+			return Ok();
+		}
+
+		public async Task<IActionResult> SetVolume(double volume)
+		{
+			await _audioController.DefaultPlaybackDevice.SetVolumeAsync(volume);
+			return Ok();
+		}
+	}
+}
---
+++ Remote.Volume/Startup.cs
@@ -0,0 +1,36 @@
+﻿using AudioSwitcher.AudioApi;
+using AudioSwitcher.AudioApi.CoreAudio;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Remote.Volume
+{
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddMvc();
+
+			services.AddTransient<IAudioController, CoreAudioController>();
+		}
+
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+
+			app.UseMvc();
+		}
+	}
+}
